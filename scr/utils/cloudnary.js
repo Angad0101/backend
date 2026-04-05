bfe0931182f5439d314574f6cbe5ec04d013cc2c
Angad nishad <angadnishad0027@gmail.com>
2026-04-05 08:52:28 +0530

    Multer and fileupload system

---
+++ scr/utils/cloudnary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudnary} from "cloudinary"
+import fs from "fs"
+
+    cloudnary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAM, 
+        api_key: CLOUDINARY_API_KEY,
+        api_secret: CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+
+
+
+
+
+const uploadCloudnary = async (localfilepath) => {
+    try {
+        if(!localfilepath) return null
+        const response = await cloudnary.uploader.upload(localfilepath, {
+            resource_type: "auto"
+        })
+        console.log("file is uploaded on cloudnary",
+            response.url);
+            return response
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove locally saved temporary file as upload operation got fail
+    }
+}
+
+export {uploadCloudnary}
