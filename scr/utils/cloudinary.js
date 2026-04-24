3c434c14e145c2ace81f22d76abb05b810a2e371
Angad nishad <angadnishad0027@gmail.com>
2026-04-24 09:48:59 +0530

    cloudnary error solve

--- scr/utils/cloudinary.js
+++ scr/utils/cloudinary.js
@@ -2,12 +2,10 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
     cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAM, 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
-
-
 
 
 
